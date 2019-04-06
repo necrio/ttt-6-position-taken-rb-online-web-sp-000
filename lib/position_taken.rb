754268655fe67dfd4014a895fcad7492abacd3b5
Keith Isham <kisham8899@gmail.com>
2019-04-06 18:47:31 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,11 @@
 # code your #position_taken? method here!
+
+def position_taken?(board, index)
+#array[index_number]
+board[index] == " "
+
+board[index] == nil
+board[index] == 0
+board[index] == 'X' || board[index] == 'O'
+
+end
