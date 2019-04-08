66417ca4d60b770da285a11c6cc8001334b7ee1b
Keith Isham <kisham8899@gmail.com>
2019-04-08 10:51:35 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -3,9 +3,15 @@
 def position_taken?(board, index)
 #array[index_number]
 board[index] == " "
+<<<<<<< HEAD
 
 board[index] == nil
 board[index] == 0
 board[index] == 'X' || board[index] == 'O'
+=======
+board[index] == nil
+board[index] == 0
+!(board[index].nil? || )
+>>>>>>> abde3c79c97f9658d63df96b9c3086c965681ef1
 
 end
